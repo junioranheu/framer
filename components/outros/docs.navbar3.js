019c983177581ab8a80448a1a8c2a4ce8997b4fd
junioranheu <junioranheu@gmail.com>
2022-03-17 15:58:22 -0300

    ajustar responsividade do doc.module.css

--- components/outros/docs.navbar3.js
+++ components/outros/docs.navbar3.js
@@ -2,6 +2,38 @@ import Link from 'next/link';
 import Styles from '../../styles/docs.navbar3.module.css';
 
 export default function Navbar1() {
+    const itens = [
+        {
+            item: 'On this page', subItens: []
+        },
+
+        {
+            item: 'Overview', subItens: [
+                { item: 'Animations', url: '/docs/xxx' },
+                { item: 'Gestures', url: '/docs/xxx' },
+                { item: 'Variants', url: '/docs/xxx' },
+                { item: 'Server-side rendering', url: '/docs/xxx' },
+                { item: 'MotionValues', url: '/docs/xxx' },
+                { item: 'Scroll-based animations', url: '/docs/xxx' }
+            ]
+        },
+
+        {
+            item: 'Quick Start', subItens: [
+                { item: 'Installation', url: '/docs/xxx' },
+                { item: 'Importing', url: '/docs/xxx' }
+            ]
+        },
+
+        {
+            item: 'Topics', subItens: [
+                { item: 'Animation', url: '/docs/xxx' },
+                { item: 'Gestures', url: '/docs/xxx' },
+                { item: 'MotionValue', url: '/docs/xxx' }
+            ]
+        }
+    ]
+
     return (
         <nav className={Styles.navbar}>
             <Link href='/'><a>Overview</a></Link>
