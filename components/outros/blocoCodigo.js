952c7c1e2a6c2f9b5a6796179f84238ff2d0b47b
junioranheu <junioranheu@gmail.com>
2022-03-18 20:35:38 -0300

    blocoCodigo.js

--- components/outros/blocoCodigo.js
+++ components/outros/blocoCodigo.js
@@ -2,13 +2,15 @@ import { CopyBlock, tomorrowNight } from 'react-code-blocks';
 
 export default function BlocoCodigo({ code }) {
     return (
-        <CopyBlock
-            text={code}
-            language={'javascript'}
-            showLineNumbers={false}
-            startingLineNumber={0}
-            theme={tomorrowNight}
-            codeBlock
-        />
+        <code>
+            <CopyBlock
+                text={code}
+                language={'javascript'}
+                showLineNumbers={false}
+                startingLineNumber={0}
+                theme={tomorrowNight}
+                codeBlock
+            />
+        </code>
     );
 }
