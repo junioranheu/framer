a0e67f8c5331dfa253288cc5fd1b38197898f378
junioranheu <junioranheu@gmail.com>
2022-03-19 02:02:00 -0300

    melhorias em sidebar.js

--- utils/dados/docs.exemplos.js
+++ utils/dados/docs.exemplos.js
@@ -1,119 +1,123 @@
 function itens() {
     const itens = [
         {
-            titulo: 'Visão geral',
+            titulo: 'Animação',
             texto: '',
             isTopico: true,
             url: '',
             codigo: ``
         },
         {
+            titulo: 'Keyframes',
+            texto: '',
+            isTopico: true,
+            url: '',
+            codigo: `<motion.div animate={{ x: 100 }} />`
+        },
+        {
+            titulo: 'Variantes',
+            texto: '',
+            isTopico: true,
+            url: '',
+            codigo: `<motion.div animate={{ x: 100 }} />`
+        },
+        {
             titulo: 'Animações',
+            texto: '',
+            isTopico: true,
+            url: '',
+            codigo: `<motion.div animate={{ x: 100 }} />`
+        },
+        {
+            titulo: 'Arrastar',
+            texto: '',
+            isTopico: true,
+            url: '',
+            codigo: `<motion.div animate={{ x: 100 }} />`
+        },
+        {
+            titulo: 'MotionValues',
+            texto: '',
+            isTopico: true,
+            url: '',
+            codigo: `<motion.div animate={{ x: 100 }} />`
+        },
+        {
+            titulo: 'Animações acionadas por rolagem',
+            texto: '',
+            isTopico: true,
+            url: '',
+            codigo: `<motion.div animate={{ x: 100 }} />`
+        },
+        {
+            titulo: 'Animações com links de rolagem',
+            texto: '',
+            isTopico: true,
+            url: '',
+            codigo: `<motion.div animate={{ x: 100 }} />`
+        },
+        {
+            titulo: 'Animações de saída',
+            texto: '',
+            isTopico: true,
+            url: '',
+            codigo: `<motion.div animate={{ x: 100 }} />`
+        },
+        {
+            titulo: 'Layout',
+            texto: '',
+            isTopico: true,
+            url: '',
+            codigo: `<motion.div animate={{ x: 100 }} />`
+        },
+        {
+            titulo: 'Layout compartilhado',
+            texto: '',
+            isTopico: true,
+            url: '',
+            codigo: `<motion.div animate={{ x: 100 }} />`
+        },
+        {
+            titulo: 'Linha de contorno',
+            texto: '',
+            isTopico: true,
+            url: '',
+            codigo: `<motion.div animate={{ x: 100 }} />`
+        },
+        {
+            titulo: 'Caminho',
+            texto: '',
+            isTopico: true,
+            url: '',
+            codigo: `<motion.div animate={{ x: 100 }} />`
+        },
+        {
+            titulo: 'Mais',
+            texto: '',
+            isTopico: true,
+            url: '',
+            codigo: `<motion.div animate={{ x: 100 }} />`
+        },
+        {
+            titulo: 'Padrões de UI',
             texto: '',
             isTopico: false,
             url: '',
             codigo: `<motion.div animate={{ x: 100 }} />`
         },
         {
-            titulo: 'Gestos',
+            titulo: 'Técnicas',
             texto: '',
             isTopico: false,
             url: '',
-            codigo: `<motion.div
-    drag="x"
-    dragConstraints={{ left: -100, right: 100 }}
-/>`
+            codigo: `<motion.div animate={{ x: 100 }} />`
         },
         {
-            titulo: 'Variantes',
+            titulo: 'Biblioteca de terceiros',
             texto: '',
             isTopico: false,
             url: '',
-            codigo: `const list = { hidden: { opacity: 0 } }
-const item = { hidden: { x: -10, opacity: 0 } }
-
-return (
-    <motion.ul animate="hidden" variants={list}>
-    <motion.li variants={item} />
-    <motion.li variants={item} />
-    <motion.li variants={item} />
-    </motion.ul>
-)`
-        },
-        {
-            titulo: 'Renderização do lado do servidor',
-            texto: '',
-            isTopico: false,
-            url: '',
-            codigo: `<motion.div initial={false} animate={{ x: 100 }} />`
-        },
-        {
-            titulo: 'Animações baseadas em scroll',
-            texto: '',
-            isTopico: false,
-            url: '',
-            codigo: `const x = useMotionValue(0)
-const opacity = useTransform(x, [-100, 0, 100], [0, 1, 0])
-
-return <motion.div drag="x" style={{ x, opacity }} />`
-        },
-        {
-            titulo: 'Início rápido',
-            texto: '',
-            isTopico: true,
-            url: '',
-            codigo: ``
-        },
-        {
-            titulo: 'Instalação',
-            texto: '',
-            isTopico: false,
-            url: '',
-            codigo: `npm install framer-motion`
-        },
-        {
-            titulo: 'Importar',
-            texto: '',
-            isTopico: false,
-            url: '',
-            codigo: `import { motion } from "framer-motion"`
-        },
-        {
-            titulo: 'Tópicos',
-            texto: '',
-            isTopico: true,
-            url: '',
-            codigo: ``
-        },
-        {
-            titulo: 'Animação',
-            texto: '',
-            isTopico: false,
-            url: '',
-            codigo: `const variants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1 },
-}
-     
-return (
-    <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={variants}
-    />
-)`
-        },
-        {
-            titulo: 'Gestos',
-            texto: '',
-            isTopico: false,
-            url: '',
-            codigo: `<motion.div
-    drag="x"
-    dragConstraints={{ left: -100, right: 100 }}
-    whileHover={{ scale: 1.1 }}
-    whileTap={{ scale: 0.9 }}
-/>`
+            codigo: `<motion.div animate={{ x: 100 }} />`
         }
     ]
 
