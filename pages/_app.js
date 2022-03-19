f4a1b6905c213dd9b5a21ad12cce475ce7a9c171
junioranheu <junioranheu@gmail.com>
2022-03-19 13:57:26 -0300

    ajustar title

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,17 @@ export default function App({ Component, pageProps, ...appProps }) {
     const [url, setUrl] = useState('');
 
     useEffect(() => {
+        function ajustarUrl(url) {
+            let urlAjustada = url.split('/').pop().trim();
+            urlAjustada = urlAjustada.charAt(0).toUpperCase() + urlAjustada.slice(1);
+            return urlAjustada;
+        }
+
+        // Setar url no Hook, para usar em verificarLayout();
         setUrl(appProps.router.pathname);
+
+        // Título da página;
+        document.title = `${ajustarUrl(appProps.router.pathname)} — Framer — junioranheu`;
     }, [appProps]);
 
     function verificarLayout() {
