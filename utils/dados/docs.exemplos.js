4ff6591e007b5beda40d3cdb20d1abbc1be6b31e
junioranheu <junioranheu@gmail.com>
2022-03-19 11:55:49 -0300

    docs.exemplos.js

--- utils/dados/docs.exemplos.js
+++ utils/dados/docs.exemplos.js
@@ -38,7 +38,26 @@ export const MyComponent = () => (
             texto: '',
             isTopico: true,
             url: '',
-            codigo: `<motion.div animate={{ x: 100 }} />`,
+            codigo: `import { motion } from "framer-motion"
+
+const variants = {
+    open: { opacity: 1, x: 0 },
+    closed: { opacity: 0, x: "-100%" },
+}
+
+export const MyComponent = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+    <motion.nav
+        animate={isOpen ? "open" : "closed"}
+        variants={variants}
+    >
+        <Toggle onClick={() => setIsOpen(isOpen => !isOpen)} />
+        <Items />
+    </motion.nav>
+    )
+}`,
             iframe: ''
         },
         {
@@ -46,87 +65,142 @@ export const MyComponent = () => (
             texto: '',
             isTopico: true,
             url: '',
-            codigo: `<motion.div animate={{ x: 100 }} />`,
-            iframe: ''
+            codigo: `import { motion } from "framer-motion"
+
+export const MyComponent = () => (
+    <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+    />
+)`,
+            iframe: 'https://codesandbox.io/embed/framer-motion-gesture-animations-8c5y3?view=preview&codemirror=1&hidedevtools=1&hidenavigation=1&theme=dark'
         },
         {
             titulo: 'Arrastar',
             texto: '',
             isTopico: true,
             url: '',
-            codigo: `<motion.div animate={{ x: 100 }} />`,
-            iframe: ''
+            codigo: `import { motion } from "framer-motion"
+
+export const MyComponent = () => (
+    <motion.div
+        drag
+        dragConstraints={{
+            top: -50,
+            left: -50,
+            right: 50,
+            bottom: 50,
+        }}
+    />
+)`,
+            iframe: 'https://codesandbox.io/embed/framer-motion-drag-with-constraints-pjn6t?view=preview&codemirror=1&hidedevtools=1&hidenavigation=1&theme=dark'
         },
         {
             titulo: 'MotionValues',
             texto: '',
             isTopico: true,
             url: '',
-            codigo: `<motion.div animate={{ x: 100 }} />`,
-            iframe: ''
+            codigo: `import { motion, useMotionValue, useTransform } from "framer-motion"
+
+export const MyComponent = () => {
+    const x = useMotionValue(0)
+    const background = useTransform(
+        x,
+        [-100, 0, 100],
+        ["#ff008c", "#7700ff", "rgb(230, 255, 0)"]
+    )
+
+    return (
+        <motion.div style={{ background }}>
+        <motion.div
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+            style={{ x }}
+        >
+            <Icon x={x} />
+        </motion.div>
+        </motion.div>
+    )
+}`,
+            iframe: 'https://codesandbox.io/embed/framer-motion-path-drawing-drag-and-usetransform-jnqk2?view=preview&codemirror=1&hidedevtools=1&hidenavigation=1&theme=dark'
         },
         {
             titulo: 'Animações acionadas por rolagem',
             texto: '',
             isTopico: true,
             url: '',
-            codigo: `<motion.div animate={{ x: 100 }} />`,
-            iframe: ''
+            codigo: `<motion.div
+    initial={{ opacity: 0 }}
+    whileInView={{ opacity: 1 }}
+    viewport={{ once: true }}
+/>`,
+            iframe: 'https://codesandbox.io/embed/framer-motion-5-3-scroll-triggered-animations-2hbg5?fontsize=14&hidenavigation=1&theme=dark&view=preview&codemirror=1&hidedevtools=1'
         },
         {
             titulo: 'Animações com links de rolagem',
             texto: '',
             isTopico: true,
             url: '',
-            codigo: `<motion.div animate={{ x: 100 }} />`,
-            iframe: ''
+            codigo: `import { motion, useViewportScroll } from "framer-motion"
+
+export const CircleIndicator = () => {
+    const { scrollYProgress } = useViewportScroll()
+
+    return (
+    <motion.path
+        d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
+        style={{ pathLength: scrollYProgress }}
+    />
+    )
+}`,
+            iframe: 'https://codesandbox.io/embed/framer-motion-viewport-scroll-and-svg-path-animation-mwi35?view=preview&codemirror=1&hidedevtools=1&hidenavigation=1&theme=dark'
         },
+        // {
+        //     titulo: 'Animações de saída',
+        //     texto: '',
+        //     isTopico: true,
+        //     url: '',
+        //     codigo: `<motion.div animate={{ x: 100 }} />`,
+        //     iframe: ''
+        // },
+        // {
+        //     titulo: 'Layout',
+        //     texto: '',
+        //     isTopico: true,
+        //     url: '',
+        //     codigo: `<motion.div animate={{ x: 100 }} />`,
+        //     iframe: ''
+        // },
+        // {
+        //     titulo: 'Layout compartilhado',
+        //     texto: '',
+        //     isTopico: true,
+        //     url: '',
+        //     codigo: `<motion.div animate={{ x: 100 }} />`,
+        //     iframe: ''
+        // },
+        // {
+        //     titulo: 'Linha de contorno',
+        //     texto: '',
+        //     isTopico: true,
+        //     url: '',
+        //     codigo: `<motion.div animate={{ x: 100 }} />`,
+        //     iframe: ''
+        // },
+        // {
+        //     titulo: 'Caminho',
+        //     texto: '',
+        //     isTopico: true,
+        //     url: '',
+        //     codigo: `<motion.div animate={{ x: 100 }} />`,
+        //     iframe: ''
+        // },
         {
-            titulo: 'Animações de saída',
-            texto: '',
+            titulo: 'Outros',
+            texto: ' ',
             isTopico: true,
             url: '',
-            codigo: `<motion.div animate={{ x: 100 }} />`,
-            iframe: ''
-        },
-        {
-            titulo: 'Layout',
-            texto: '',
-            isTopico: true,
-            url: '',
-            codigo: `<motion.div animate={{ x: 100 }} />`,
-            iframe: ''
-        },
-        {
-            titulo: 'Layout compartilhado',
-            texto: '',
-            isTopico: true,
-            url: '',
-            codigo: `<motion.div animate={{ x: 100 }} />`,
-            iframe: ''
-        },
-        {
-            titulo: 'Linha de contorno',
-            texto: '',
-            isTopico: true,
-            url: '',
-            codigo: `<motion.div animate={{ x: 100 }} />`,
-            iframe: ''
-        },
-        {
-            titulo: 'Caminho',
-            texto: '',
-            isTopico: true,
-            url: '',
-            codigo: `<motion.div animate={{ x: 100 }} />`,
-            iframe: ''
-        },
-        {
-            titulo: 'Mais',
-            texto: '',
-            isTopico: true,
-            url: '',
-            codigo: `<motion.div animate={{ x: 100 }} />`,
+            codigo: ``,
             iframe: ''
         },
         {
@@ -134,7 +208,7 @@ export const MyComponent = () => (
             texto: '',
             isTopico: false,
             url: '',
-            codigo: `<motion.div animate={{ x: 100 }} />`,
+            codigo: ``,
             iframe: ''
         },
         {
@@ -142,7 +216,7 @@ export const MyComponent = () => (
             texto: '',
             isTopico: false,
             url: '',
-            codigo: `<motion.div animate={{ x: 100 }} />`,
+            codigo: ``,
             iframe: ''
         },
         {
@@ -150,7 +224,7 @@ export const MyComponent = () => (
             texto: '',
             isTopico: false,
             url: '',
-            codigo: `<motion.div animate={{ x: 100 }} />`,
+            codigo: ``,
         }
     ]
 
