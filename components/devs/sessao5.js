77fc1896e46c7699be4e2d4685393c753e04cd3e
junioranheu <junioranheu@gmail.com>
2022-03-21 13:14:42 -0300

    finalizar sessao5.js de /devs

--- components/devs/sessao5.js
+++ components/devs/sessao5.js
@@ -1,4 +1,6 @@
+import Image from 'next/image';
 import React from 'react';
+import Chaleco from '../../static/pages/devs/chaleco.webp';
 import Styles from '../../styles/landingPage.module.css';
 import CardGigante from '../outros/cardGigante.js';
 
@@ -6,12 +8,22 @@ export default function Sessao4() {
     return (
         <section className={`${Styles.principal} ${Styles.corSecundaria}`}>
             {/* Comentário */}
-            <div className={`${Styles.sessaoTituloPrincipal} ${Styles.margemTopG}`}>
-                <span className={Styles.tituloPrincipalMedio}>Sessao 5</span>
+            <div className={`${Styles.divUsuarioComentario} ${Styles.margemTopG}`}>
+                <div>
+                    <Image src={Chaleco} alt='' />
+                </div>
+
+                <div>
+                    <span>Israel Chaleco</span>
+                    <span>Estudante de Ciência da Computação em Santiago</span>
+                </div>
             </div>
 
             <div className={Styles.margemTopP}>
-                <span className={Styles.descricaoTituloPrincipal}>Explore tudo que é possível ser feito com o Framer.</span>
+                <span className={Styles.descricaoTituloPrincipal}>“Com a possibilidade de programar qualquer coisa</span>
+                <span className={Styles.descricaoTituloPrincipal}>com a ajuda do Framer, foi possível criar componentes</span>
+                <span className={Styles.descricaoTituloPrincipal}>super dinâmicos muito facilmente — eu sei que,</span>
+                <span className={Styles.descricaoTituloPrincipal}>isso seria bem difícil fazendo à mão ou usando outra ferramenta.”</span>
             </div>
 
             {/* Card gigante */}
