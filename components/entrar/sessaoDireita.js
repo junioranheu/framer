e87cec1dba71551c2ac75d4c756707da4ffa92c2
junioranheu <junioranheu@gmail.com>
2022-03-22 23:46:58 -0300

    finalizar /entrar

--- components/entrar/sessaoDireita.js
+++ components/entrar/sessaoDireita.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Video1 from '../../static/pages/entrar/video1.mp4';
 import Styles from '../../styles/entrar.module.css';
 
 export default function sessaoDireita() {
     return (
         <section className={Styles.divDireita}>
-            aaa
+            <video className={Styles.video} autoPlay loop muted playsInline disablePictureInPicture controls={false}>
+                <source src={Video1} type='video/mp4' />
+            </video>
         </section>
     )
 }
