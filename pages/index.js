a28641af62f5c23efc65a7f6d3cd258bd89ff24f
junioranheu <junioranheu@gmail.com>
2022-03-23 17:34:34 -0300

    instalar react-cool-inview e react-shimmer-effects

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
+import React, { Fragment, useEffect, useState } from 'react';
+import useInView from 'react-cool-inview';
+import { ShimmerThumbnail } from 'react-shimmer-effects';
 import Sessao1 from '../components/landingPage/sessao1.js';
-import Sessao2 from '../components/landingPage/sessao2.js';
-import Sessao3 from '../components/landingPage/sessao3.js';
-import Sessao4 from '../components/landingPage/sessao4.js';
-import Sessao5 from '../components/landingPage/sessao5.js';
-import Sessao6 from '../components/landingPage/sessao6.js';
 import { Aviso } from '../components/outros/aviso';
 import EmojiAleatorio from '../utils/outros/emojiAleatorio';
+const Sessao2 = dynamic(() => import('../components/landingPage/sessao2.js'));
+const Sessao3 = dynamic(() => import('../components/landingPage/sessao3.js'));
+const Sessao4 = dynamic(() => import('../components/landingPage/sessao4.js'));
+const Sessao5 = dynamic(() => import('../components/landingPage/sessao5.js'));
+const Sessao6 = dynamic(() => import('../components/landingPage/sessao6.js'));
 
 export default function Index() {
     const [mostrarAviso, isMostrarAviso] = useState(true);
@@ -23,14 +26,43 @@ export default function Index() {
         }
     }, [mostrarAviso]);
 
+    const [exibirConteudo, setExibirConteudo] = useState(false);
+    const { observe, inView } = useInView({
+        unobserveOnEnter: true, // Executar apenas uma vez, para não ficar 'escondendo' o conteúdo depois;
+        onEnter: (() => handleExibirConteudo())
+    });
+
+    function handleExibirConteudo() {
+        // console.log('Scroll feito, exibir conteúdo');
+
+        setTimeout(function () {
+            // console.log('setExibirConteudo(true)');
+            setExibirConteudo(true);
+        }, 1000);
+    }
+
     return (
         <section>
             <Sessao1 />
             <Sessao2 />
-            <Sessao3 />
-            <Sessao4 />
-            <Sessao5 />
-            <Sessao6 />
+
+            {/* Lazy load on scroll - https://www.youtube.com/watch?v=_cccpx2Bw_g&t=463s&ab_channel=BetterDev */}
+            <div ref={observe}>
+                {(inView && !exibirConteudo) && (
+                    <Fragment>
+                        <ShimmerThumbnail rounded />
+                    </Fragment>
+                )}
+
+                {(inView && exibirConteudo) && (
+                    <Fragment>
+                        <Sessao3 />
+                        <Sessao4 />
+                        <Sessao5 />
+                        <Sessao6 />
+                    </Fragment>
+                )}
+            </div>
         </section>
     )
 }
