8982bd579737275011f53e0ab7a5c8ba6b29f299
junioranheu <junioranheu@gmail.com>
2022-03-23 19:49:25 -0300

    ajustar docs.navbar2.js: incluir simulação de busca

--- components/outros/docs.navbar2.js
+++ components/outros/docs.navbar2.js
@@ -1,29 +1,47 @@
 import Link from 'next/link';
+import React, { useState } from 'react';
 import Lupa from '../../components/svg/lupa';
 import Styles from '../../styles/docs.navbar2.module.css';
 import Framer from '../svg/framer';
+import Xis from '../svg/xis';
 import Botao from './botao';
 
 export default function Navbar2() {
+    const [isLupa, setIsLupa] = useState(false);
+
+    function handleLupa() {
+        setIsLupa(!isLupa);
+    }
+
     return (
         <nav className={Styles.navbar}>
-            <div>
-                <div>
-                    <Link href='/'><a className={Styles.iconeCorInvertida}><Framer width='0.9rem' />&nbsp;&nbsp;Framer</a></Link>
-                    <Link href='/docs'><a>Documentação</a></Link>
-                    <Link href='/devs'><a>Desenvolvedores</a></Link>
-                    {/* <Link href='/precos'><a>Preços</a></Link> */}
-                    <Link href='/'><a><Lupa /></a></Link>
-                </div>
+            {!isLupa ? (
+                <div className={Styles.wrapper}>
+                    <div className={Styles.divEsquerda}>
+                        <Link href='/'><a className={Styles.iconeCorInvertida}><Framer width='0.9rem' />&nbsp;&nbsp;Framer</a></Link>
+                        <Link href='/docs'><a>Documentação</a></Link>
+                        <Link href='/devs'><a>Desenvolvedores</a></Link>
+                        {/* <Link href='/precos'><a>Preços</a></Link> */}
+                        <a onClick={() => handleLupa()}><Lupa /></a>
+                    </div>
 
-                <div>
-                    <Link href='/entrar'><a>Entrar</a></Link>
+                    <div className={Styles.divDireita}>
+                        <Link href='/entrar'><a>Entrar</a></Link>
 
-                    <span className={Styles.margemBotao}>
-                        <Botao texto={'Crie sua conta'} url={'/criar-conta'} isNovaAba={false} Svg='' />
-                    </span>
+                        <span className={Styles.margemBotao}>
+                            <Botao texto={'Crie sua conta'} url={'/criar-conta'} isNovaAba={false} Svg='' />
+                        </span>
+                    </div>
                 </div>
-            </div>
+            ) : (
+                <div className={Styles.divLupa}>
+                    <div>
+                        <Lupa />
+                        <input className={Styles.input} type='text' placeholder='Busque algo aqui...' />
+                        <button className={Styles.botaoXis} onClick={() => handleLupa()}><Xis width='1.5rem' /></button>
+                    </div>
+                </div>
+            )}
         </nav>
     )
 }
