b1ff8dd04012da1167ee5caac7d14b1773e10ccf
junioranheu <junioranheu@gmail.com>
2022-03-23 19:51:16 -0300

    corrigir bug strokeWidth e strokeLinecap

--- components/svg/xis.js
+++ components/svg/xis.js
@@ -2,7 +2,7 @@
 export default function Xis({width}) {
     return (
         <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' role='presentation' width={width}>
-            <path d='M17 7L7 17M7 7l5.03 5.03L17 17' fill='transparent' stroke-width='2' stroke='currentColor' stroke-linecap='round'></path>
+            <path d='M17 7L7 17M7 7l5.03 5.03L17 17' fill='transparent' strokeWidth='2' stroke='currentColor' strokeLinecap='round'></path>
         </svg>
     )
 }
